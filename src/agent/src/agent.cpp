b1261585a54bb74d7195b4d1f88a7134092bb0be
liguangpeng <liguangpeng@roboway.cn>
2019-05-22 20:09:11 +0800

    add agent

--- src/agent/src/agent.cpp
+++ src/agent/src/agent.cpp
@@ -1,6 +1,8 @@
 #include <ros/ros.h>
 #include <agent/commond.h>
 
+#include <QProcess>
+
 class Agent
 {
 public:
@@ -11,18 +13,57 @@ private:
     ros::NodeHandle node;
     bool commondCallback(agent::commond::Request & request, agent::commond::Response & response);
     std::string pathString;
-    
+    QProcess *slamProcess;
+    QProcess *moveBaseProcess;
+    QProcess *clientProcess;
+    int status;
 };
 
 bool Agent::commondCallback(agent::commond::Request & request, agent::commond::Response & response)
 {
     ROS_INFO("here  %d", request.type);
-    system("touch a.cc");
 
     switch(request.type)
     {
         case 0:
-
+            if(QProcess::NotRunning == slamProcess->state())
+            {
+                slamProcess->start("roslaunch bringup slam.launch");
+                status = 1;
+            }
+            response.returntype = true;
+            break;
+        case 1:
+            if(QProcess::NotRunning != slamProcess->state())
+            {
+                slamProcess->terminate();
+                slamProcess->waitForFinished();
+                status = 0;
+            }
+            response.returntype = true;
+            break;
+        case 2:
+            if(QProcess::NotRunning == moveBaseProcess->state())
+            {
+                moveBaseProcess->start("roslaunch bringup move_base.launch");
+                clientProcess->start("roslaunch bringup client.launch");
+                status = 2;
+            }
+            response.returntype = true;
+            break;
+        case 3:
+            if(QProcess::NotRunning != moveBaseProcess->state())
+            {
+                clientProcess->terminate();
+                clientProcess->waitForFinished();
+                moveBaseProcess->terminate();
+                moveBaseProcess->waitForFinished();
+                status = 0;
+            }
+            response.returntype = true;
+            break;
+        case 4:
+            response.returntype = status;
             break;
         default:
             break;
@@ -32,12 +73,32 @@ bool Agent::commondCallback(agent::commond::Request & request, agent::commond::R
 
 Agent::Agent()
 {
-    ros::param::get("~path", pathString);
-    ROS_INFO_STREAM(pathString);
+    status = 0;
+    slamProcess = new QProcess();
+    moveBaseProcess = new QProcess();
+    clientProcess = new QProcess();
 }
 Agent::~Agent()
 {
-    
+    if(QProcess::NotRunning != slamProcess->state())
+    {
+        slamProcess->terminate();
+        slamProcess->waitForFinished();
+    }
+    delete slamProcess;
+    if(QProcess::NotRunning != clientProcess->state())
+    {
+        clientProcess->terminate();
+        clientProcess->waitForFinished();
+    }
+    delete clientProcess;
+
+    if(QProcess::NotRunning != moveBaseProcess->state())
+    {
+        moveBaseProcess->terminate();
+        moveBaseProcess->waitForFinished();
+    }
+    delete moveBaseProcess;
 }
 
 void Agent::exec()
--- src/bringup/script/save_map.sh
+++ src/bringup/script/save_map.sh
@@ -11,7 +11,7 @@ roscd bringup/map
 rosrun map_server map_saver -f $1
 cp $1.pgm $1_modify.pgm
 cp $1.yaml $1_modify.yaml
-sed -i s/$1/$1_modify/ $1_modify.yaml
+sed -i s/$1.pgm/$1_modify.pgm/ $1_modify.yaml
 
 roscd bringup/script
 python modify_launch.py $1
